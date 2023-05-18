0e98f13a9f770d480ef1797ba5c1b97a3b5e2084
Ali Yousefi <ali.visual.studio@gmail.com>
2023-05-18 11:49:32 +0330

    Refactor structure

---
+++ src/CSharp/EasyMicroservices.ServiceContracts/ValidationContract.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceContracts
+{
+    /// <summary>
+    /// data of validation means
+    /// </summary>
+    public class ValidationContract
+    {
+        /// <summary>
+        /// message of validation
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// details for debugger
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
