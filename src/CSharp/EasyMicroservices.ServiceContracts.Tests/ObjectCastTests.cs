36ee558dce614c2ab2e476cc0d095e7d73c9871f
Ali Yousefi <ali.visual.studio@gmail.com>
2023-08-29 17:48:44 +0330

    check is success tests

--- src/CSharp/EasyMicroservices.ServiceContracts.Tests/ObjectCastTests.cs
+++ src/CSharp/EasyMicroservices.ServiceContracts.Tests/ObjectCastTests.cs
@@ -27,6 +27,30 @@
         }
 
         [Fact]
+        public void SuccessMessageContractGenericToMessageContract()
+        {
+            object contract = (MessageContract<string>)"Hello World";
+            MessageContract converted = contract.ToContract();
+            AssertMessageContract(contract, converted);
+        }
+
+        [Fact]
+        public void SuccessMessageContractGenericToMessageContractGeneric()
+        {
+            object contract = (MessageContract<string>)"Hello World";
+            MessageContract<string> converted = contract.ToContract<string>();
+            AssertMessageContract<string>(contract as MessageContract<string>, converted);
+        }
+
+        [Fact]
+        public void SuccessMessageContractGenericToMessageContractGenericEndUserMessage()
+        {
+            object contract = (MessageContract<string>)("Hello World", "End user text");
+            MessageContract<string> converted = contract.ToContract<string>();
+            AssertMessageContract<string>(contract as MessageContract<string>, converted);
+        }
+
+        [Fact]
         public void MessageGenericToListMessageContract()
         {
             object contract = (MessageContract<int>)FailedReasonType.InternalError;
@@ -34,6 +58,13 @@
             AssertMessageContract(contract, converted);
         }
 
+        [Fact]
+        public void SuccessMessageGenericToListMessageContract()
+        {
+            object contract = (MessageContract<int>)157;
+            ListMessageContract<string> converted = contract.ToListContract<string>();
+            AssertMessageContract(contract, converted);
+        }
 
         [Fact]
         public void ListMessageContractToMessageContract()
